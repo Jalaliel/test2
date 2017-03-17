152a58f656b02bc509bc83b77ba047f00413e139
Jalaliel <chloe.decultot@gmail.com>
2017-03-17 11:19:33 +0100

    Initiation C++

---
+++ testGit/src/compte.cpp
@@ -0,0 +1,15 @@
+/*
+ * compte.cpp
+ *
+ *  Created on: 17 mars 2017
+ *      Author: cdecultot
+ */
+#include "compte.hpp"
+#include <iostream>
+
+Compte::Compte(const float montantInit){
+	solde=montantInit;
+}
+
+
+
---
+++ testGit/src/compte.hpp
@@ -0,0 +1,10 @@
+#ifndef __COMPTE_HPP__
+#define __COMPTE_HPP__
+
+class Compte{
+private:
+	float solde;
+public:
+	Compte(const float);
+
+};
